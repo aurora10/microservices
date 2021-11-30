e8107dc9afbde3672c61b1c00d520b654ca4f328
rob <rob@robs-iMac.local>
2021-11-30 19:31:56 +0100

    Middleware

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,4 @@ COPY . .
 RUN composer install 
 
 CMD php artisan serve --host=0.0.0.0
-EXPOSE 8000
+EXPOSE 8000 
--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('users', 'UserController@index');
+// Route::get('users', 'UserController@index');
+// Route::get('users/{id}', 'UserController@show');
+// Route::post('users', 'UserController@store');
+// Route::put('users/{id}', 'UserController@update');
+// Route::delete('users/{id}', 'UserController@destroy');
+
+
+
+
+Route::post('login', 'AuthController@login');
+
+
+
+Route::group(['middleware' => 'auth:api'], function() {
+    Route::apiResource('users', 'UserController');
+});
+
