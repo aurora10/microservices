875525d9da0ce26314dd02469af4b7a60661baf9
rob <rob@robs-iMac.local>
2021-12-10 17:07:44 +0100

    OrderTotal

--- routes/api.php
+++ routes/api.php
@@ -36,7 +36,7 @@ Route::group(['middleware' => 'auth:api'], function() {
     Route::apiResource('users', 'UserController');
     Route::apiResource('roles', 'RoleController');
     Route::apiResource('products', 'ProductController');
-    
+    Route::apiResource('orders', 'OrderController')->only('index','show');
 
 });
 
