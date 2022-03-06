15f976092f4af910bc16073b659888d624077da6
aurora10 <aurora10@gmail.com>
2022-03-06 12:09:20 +0100

    Nested Routes

--- routes/api.php
+++ routes/api.php
@@ -3,74 +3,119 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-// Route::get('users', 'UserController@index');
-// Route::get('users/{id}', 'UserController@show');
-// Route::post('users', 'UserController@store');
-// Route::put('users/{id}', 'UserController@update');
-// Route::delete('users/{id}', 'UserController@destroy');
-
 
 //Common routes
-Route::post('login', 'AuthController@login');
-Route::post('register', 'AuthController@register');
+// Route::post('login', 'AuthController@login');
+// Route::post('register', 'AuthController@register');
 
-Route::group(['middleware' => 'auth:api',
-],
-function() {
+// Route::group(['middleware' => 'auth:api',
+// ],
+// function() {
+//             Route::get('user', 'AuthController@user');
+//             Route::put('users/info', 'AuthController@updateInfo');
+//             Route::put('users/password', 'AuthController@updatePassword');
+// });
+
+
+// ===================== ADMIN NEW ============================================
+
+Route::prefix('admin')->group(function(){
+    Route::post('login', 'AuthController@login');
+    Route::post('register', 'AuthController@register');
+    Route::post('logout', 'AuthController@logout'); //logout
+
+    Route::middleware(['auth:api', 'scope:admin'])->group(function(){
             Route::get('user', 'AuthController@user');
             Route::put('users/info', 'AuthController@updateInfo');
             Route::put('users/password', 'AuthController@updatePassword');
+
+            Route::namespace('Admin')->group(function(){
+                    //Route::post('logout', 'AuthController@logout'); //logout
+                    Route::get('chart', 'DashboardController@chart');
+
+                    Route::post('upload', 'ImageController@upload');
+                    Route::get('export', 'OrderController@export');
+
+                    Route::apiResource('users', 'UserController');
+                    Route::apiResource('roles', 'RoleController');
+                    Route::apiResource('products', 'ProductController');
+                    Route::apiResource('orders', 'OrderController')->only('index','show');
+                    Route::apiResource('permissions', 'PermissionController')->only('index');
+            });
+    });
 });
 
-//Admin
-Route::group(['middleware' => ['auth:api', 'scope:admin'],
-               'prefix' => 'admin',
-                'namespace' => 'Admin'],
-                function() {
+
+//=====================END ADMIN NEW
+
+//Admin old
+// Route::group(['middleware' => ['auth:api', 'scope:admin'],
+//                'prefix' => 'admin',
+//                 'namespace' => 'Admin'],
+//                 function() {
+//     Route::post('logout', 'AuthController@logout');
+//     Route::get('chart', 'DashboardController@chart');
+
+
+//     Route::post('upload', 'ImageController@upload');
+//     Route::get('export', 'OrderController@export');
+
+
+//     Route::apiResource('users', 'UserController');
+//     Route::apiResource('roles', 'RoleController');
+//     Route::apiResource('products', 'ProductController');
+//     Route::apiResource('orders', 'OrderController')->only('index','show');
+//     Route::apiResource('permissions', 'PermissionController')->only('index');
+
+// });
+
+
+// ===================== INFLUENCER NEW ============================================
+
+Route::prefix('influencer')->group(function(){
+    Route::post('login', 'AuthController@login');
+    Route::post('register', 'AuthController@register');
     Route::post('logout', 'AuthController@logout');
-    Route::get('chart', 'DashboardController@chart');
+
+    Route::get('products', 'Influencer\ProductController@index');
 
 
-    Route::post('upload', 'ImageController@upload');
-    Route::get('export', 'OrderController@export');
+    Route::middleware(['auth:api', 'scope:influencer'])->group(function(){
+            Route::get('user', 'AuthController@user');
+            Route::put('users/info', 'AuthController@updateInfo');
+            Route::put('users/password', 'AuthController@updatePassword');
 
-
-    Route::apiResource('users', 'UserController');
-    Route::apiResource('roles', 'RoleController');
-    Route::apiResource('products', 'ProductController');
-    Route::apiResource('orders', 'OrderController')->only('index','show');
-    Route::apiResource('permissions', 'PermissionController')->only('index');
-
+            Route::namespace('Influencer')->group(function(){
+                //Route::post('logout', 'AuthController@logout'); //logout
+                Route::post('links', 'LinkController@store');
+                Route::get('stats', 'StatsController@index');
+                Route::get('rankings', 'StatsController@ranking');
+            });
+    });
 });
-//Influencer
-Route::group([
-               'prefix' => 'influencer',
-                'namespace' => 'Influencer'],
-                function() {
-                    Route::get('products', 'ProductController@index');
 
-                    Route::group([
-                        'middleware' => ['auth:api', 'scope:influencer'],
-                    ], function () {
-                            Route::post('links', 'LinkController@store');
-                            Route::get('stats', 'StatsController@index');
-                            Route::get('rankings', 'StatsController@ranking');
-                    });
+// ===================== END INFLUENCER NEW ============================================
+
+//Influencer old
+// Route::group([
+//                'prefix' => 'influencer',
+//                 'namespace' => 'Influencer'],
+//                 function() {
+//                     Route::get('products', 'ProductController@index');
+
+//                     Route::group([
+//                         'middleware' => ['auth:api', 'scope:influencer'],
+//                     ], function () {
+//                             Route::post('links', 'LinkController@store');
+//                             Route::get('stats', 'StatsController@index');
+//                             Route::get('rankings', 'StatsController@ranking');
+//                     });
 
 
 
-});
+// });
+
+
 
 //CHeckout
 Route::group([
